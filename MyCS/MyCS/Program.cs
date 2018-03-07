c7e3ab50cdc4edba1507f22c72c887c4999e6a21
Ella Stern <37144466+eisoftest@users.noreply.github.com>
2018-03-07 16:21:36 +0200

    Add files via upload

---
+++ MyCS/MyCS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MisTareas
+{
+    public class Contador
+    {
+
+        public void Cuentame(string texto)
+        {
+            if (texto == null)
+            {
+                Console.WriteLine("\nNo se acepta texto NUL!");
+                return;
+            }
+
+            texto.ToLower();
+            char[] todo = texto.ToCharArray();
+            char[] vocales = "aeiou".ToCharArray();
+            char[] consonantes = "bcdfghjlmnpqrstvzxy".ToCharArray();
+
+            int t, v = 0, c = 0;
+            t = todo.Length;
+
+            foreach (char item in todo)
+            {
+                string elem = item.ToString();
+                if (elem.IndexOfAny(vocales) > -1) v++;
+                else if (elem.IndexOfAny(consonantes) > -1) c++;
+            }
+
+            Console.WriteLine("\nLetras: {0}", v + c);
+            Console.WriteLine("Espacios: {0}", t - v - c);
+            Console.WriteLine("Vocales: {0}", v);
+            Console.WriteLine("Consonantes: {0}", c);
+        }
+    }
+
+    class MiTarea
+    {
+
+        static void Main()
+        {
+            string ejemplo = "Brasil decirme que se siente";
+            Contador obj = new Contador();
+            obj.Cuentame(ejemplo);
+            Console.ReadLine();
+        }
+    }
+}
+
