de24d63223939bb5b7e224398efa882a9a7ad909
eisoftserv <ellailona@gmail.com>
2018-03-07 17:18:22 +0200

    test b

---
+++ Subtree/Delega/Delega/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+namespace mydelegates
+{
+    public delegate void MyTool(int rep, string id);
+
+    public class Gunner
+    {
+        event MyTool Shot;
+
+        public Gunner()
+        {
+            Shot += new MyTool(OnShot);
+        }
+
+        public void Shooting(int n, string x)
+        {
+            Shot(n, x);
+        }
+
+        void OnShot(int rep, string id)
+        {
+            for (int i = 0; i < rep; i++)
+            {
+                Console.WriteLine("{0} is shooting: {1}", id, DateTime.Now.ToString());
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+
+
+
+    public class MyClass
+    {
+        static void Main()
+        {
+            Console.WriteLine("\nStart...\n");
+
+            Gunner g = new Gunner();
+            Gunner h = new Gunner();
+            g.Shooting(3, "Jim");
+            h.Shooting(2, "Dan");
+
+            Console.Write("\nEnd...");
+            Console.ReadLine();
+        }
+    }
+
+}
